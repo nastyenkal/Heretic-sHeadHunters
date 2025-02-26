f5ec1c87ffe9a06383e0c3b7bde183e481e87a76
nastyenkal <docnastyadit@gmail.com>
2025-02-26 12:26:45 +0300

    StartNastya

---
+++ Heretic-sHeadHunters/src/main/java/mephi/b23902/headhunters/heretic/sheadhunters/HereticSHeadHunters.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b23902.headhunters.heretic.sheadhunters;
+
+/**
+ *
+ * @author Настя
+ */
+public class HereticSHeadHunters {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
